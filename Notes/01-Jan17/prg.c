b22ec88887bbbc9acc051da279679070252af816
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2023-01-17 13:29:08 -0500

    jan 17

--- Notes/01-Jan17/prg.c
+++ Notes/01-Jan17/prg.c
@@ -1,19 +1,44 @@
 #include <stdio.h>
-// receives or returns, functions talking between themseleves
+/*
 
-void greetings(void); // function introduction or in C: function prototype
+ char short int long (long long)
+ float  double  (long double)
+
+
+*/
+
+
+void greetings(void);
 void goodbye(void);
-
+void line(void);
 int main(void) {
-
+   int num;
+   int value = 10;
+   float fnum;
    greetings();
+  
+   num = 25;
+   fnum = 25.123;
+
+   num = value + 32;
+
+   printf("integer num is %d, and float fnum is %f\n", num, fnum);
+   
+
+
    goodbye();
    return 0;
 }
 
 void greetings(void) {
    printf("\tHello IPC144NBB!!!!\n");
+   line();
+   putchar('\n');
 }
 void goodbye(void) {
-   printf("\tGoodbye everyone\n");
+   line();
+   printf("\tGoodbye everyone\n\n");
+}
+void line(void) {
+   printf("------------------------------------------------\n");
 }
