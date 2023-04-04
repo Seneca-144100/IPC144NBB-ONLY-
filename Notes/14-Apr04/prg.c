2e9e6000bd3308e0aa007ab4fc7758be6fe0d415
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2023-04-04 13:38:24 -0400

    algorithm

--- Notes/14-Apr04/prg.c
+++ Notes/14-Apr04/prg.c
@@ -1,48 +1,53 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
-void prnIntTable(const int t[][5], int row) {
+struct Student {
+   char name[81];
+   int stno;
+   double gpa;
+};
+void sortInts(int ar[], int size) {
    int i, j;
-   for(i = 0; i < row; i++) {  // go through the size arrays
-      for(j = 0; j < 5; j++) { // go through the elements of each arrau
-         printf("%5d%s", t[i][j], (j != 4) ? ", " : "\n");
+   for(i = 0; i < size - 1; i++) {
+      for(j = 0; j < size - i - 1; j++) {
+         if(ar[j] > ar[j + 1]) {
+            int temp = ar[j];
+            ar[j] = ar[j + 1];
+            ar[j + 1] = temp;
+         }
       }
    }
-
 }
-
-void prnTables(int b[][3][5], int boxes) {
-   for(int i = 0; i < boxes; i++) {
-      printf("Table %d =====================================\n", (i + 1));
-      prnIntTable(b[i], 3);
+void prnInts(const int a[], int size) {
+   int i;
+   for(i = 0; i < size; i++) {
+      printf("%d%s", a[i], (i != size - 1) ? ", ": "\n");
    }
 }
+void prnStds(const struct Student a[], int size) {
+   int i;
+   for(i = 0; i < size; i++) {
+      printf("%3d- %s: %d[%.1lf]\n",(i+1), a[i].name, a[i].stno, a[i].gpa);
+   }
+}
+void sortFunc(void) {
+   int a[5] = { 5, 1, 4, 2 ,8 };
+   sortInts(a, 5);
+   prnInts(a, 5);
+}
+int readStd(struct Student* s, FILE* f) {
+   int res = fscanf(f, "%[^,],%d,%lf", s->name, &s->stno, &s->gpa) == 3;
+   while(res && fgetc(f) != '\n');
+   return res;
+}
+
 
 int main(void) {
-   int a[4][3][5] =
-   {
-      {                                                 // 0
-         // 0   1     2    3    4
-         { 10,  20,  30,  40,  50 },    // 0
-         { 110, 120, 130, 140, 150 },   // 1
-         { 210, 220, 230, 240, 250 }    // 2
-      },
-      {                                                  // 1
-         { 100,  200,  300,  400,  500 },
-         { 1100, 1200, 1300, 1400, 1500 },
-         { 2100, 2200, 2300, 2400, 2500 }
-      },
-      {                                                 // 2
-         { 101,  201,  301,  401,  501 },
-         { 1101, 1201, 1301, 1401, 1501 },
-         { 2101, 2201, 2301, 2401, 2501 }
-      },
-      {                                                // 3 
-         { 102,  202,  302,  402,  502 },
-         { 1102, 1202, 1302, 1402, 1502 },
-         { 2102, 2202, 2302, 2402, 2502 }
-      }
-   };
-   prnTables(a, 4);
+   struct Student s[50];
+   int noOfStd;
+   FILE* fptr = fopen("students.csv", "r");
+   for(noOfStd = 0; readStd(&s[noOfStd], fptr); noOfStd++);
+   prnStds(s, noOfStd);
+   fclose(fptr);
    return 0;
 }
+
