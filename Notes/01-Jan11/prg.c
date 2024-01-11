c3983ae6d78612457c271b7b68b4123cd7c64fca
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2024-01-11 11:26:56 -0500

    Jan10 intro to functions

---
+++ Notes/01-Jan11/prg.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void hello(void) {
+   printf("Hello IPC144 NBB\n");
+}
+void introduce(void) {
+   printf("My name is Fardad soleimanloo\n");
+}
+int main(void) {
+   hello();
+   introduce();
+   return 0;
+}
